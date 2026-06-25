4e5686a31d184dc07bd11abe14b08f05d1e449dc
ptrck6269-debug <ptrck6269@gmail.com>
2026-06-25 15:46:54 -0400

    Update jsdemo.js

--- jsdemo.js
+++ jsdemo.js
@@ -1,1 +1,8 @@
 console.log("This is loaded from the external JS file");
+var name = "Michael";
+let school = "Mallard Creek High School";
+var fun_fact = "I do photography";
+
+console.log(`Name: ${name}`);
+console.log(`School: ${school}`);
+console.log(`Fun Fact: ${fun_fact}`);
