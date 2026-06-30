6c7d2a5e93359101a84c38f1f2d8199ed111b48e
ptrck6269-debug <ptrck6269@gmail.com>
2026-06-30 15:50:54 -0400

    Update jsdemo.js

--- jsdemo.js
+++ jsdemo.js
@@ -6,3 +6,7 @@ var fun_fact = "I do photography";
 console.log(`Name: ${name}`);
 console.log(`School: ${school}`);
 console.log(`Fun Fact: ${fun_fact}`);
+
+document.getElementById("name").innerHTML = "<b>Name: </b><i>" + name + "</i>"; 
+document.getElementById("school").innerHTML = "<b>School: </b><i>" + school + "</i>"; 
+document.getElementById("fun_fact").innerHTML = "<b>Fun Fact: </b><i>" + fun_fact + "</i>"; 
