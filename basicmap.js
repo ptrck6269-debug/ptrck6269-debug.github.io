3ce7f5bbd20f0d9ff7a57d04457e9c239e556549
ptrck6269-debug <ptrck6269@gmail.com>
2026-07-07 16:12:05 -0400

    Update basicmap.js

--- basicmap.js
+++ basicmap.js
@@ -1,4 +1,6 @@
-var map = L.map('map').setView([35.2271, -80.8431], 13);
+// Charlotte, North Carolina is located at 35.2271° N latitude and -80.8431° W longitude
+var charlotte = [35.2271, -80.8431];
+var map = L.map('map').setView(charlotte, 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
