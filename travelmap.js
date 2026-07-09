92345f18487a714e2192fee8a858c18818c823b1
ptrck6269-debug <ptrck6269@gmail.com>
2026-07-09 16:15:05 -0400

    Update travelmap.js

--- travelmap.js
+++ travelmap.js
@@ -13,7 +13,7 @@ var popupcode = `
 <iframe width="200" height="200" src="https://www.youtube.com/embed/LxiTej0JDEQ?si=eWlCB8CxOC3B_D9R" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 </center>`;
 var fp = [32.785012, -79.785655]
-var map = L.map('map').setView(coordinates, 12);
+var map = L.map('map').setView(coordinates, 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
